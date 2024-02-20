d7f534f2bac56c32de82a70801f2d7f5360b3662
Matheus Ramos Marcolino <matheus.r.marcolino@gmail.com>
2024-02-20 12:10:03 -0300

    First commit

---
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable Long id){
+        return todoService.delete(id);
+    }
+}
---
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.example.todolist.service;
+
+import com.example.todolist.entity.Todo;
+import com.example.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+
+}
